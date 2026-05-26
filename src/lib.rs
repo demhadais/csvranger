ff3d1625bfe10fe345a72370a7eba91aa5c77c7b
Ahmed Said <ahmed.said@jax.org>
2026-05-26 18:02:02 -0400

    complete first test

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,30 @@ mod tests {
         let raw_data = include_bytes!(
             "../test-data/cellranger_multi.10.0/SOD1_G93A_mouse_spinal_cord_P112_specimen_1_Multiplex_qc_library_metrics.csv"
         );
+
         let parsed_data = read_multi_row_csv(&raw_data[..]);
+        let expected_data = vec![
+            TenxCsvValue::I32(321950603),
+            TenxCsvValue::F64(0.9754667347931396),
+            TenxCsvValue::F64(0.9838510115040644),
+            TenxCsvValue::F64(0.9810434401949358),
+            TenxCsvValue::I32(16410),
+            TenxCsvValue::F64(0.18127985925840928),
+            TenxCsvValue::F64(0.6847850295990157),
+            TenxCsvValue::F64(0.39215314033749454),
+            TenxCsvValue::F64(0.9176445866138042),
+            TenxCsvValue::F64(0.07506513662283776),
+            TenxCsvValue::F64(0.4504263096534719),
+            TenxCsvValue::F64(0.657908592269355),
+            TenxCsvValue::F64(0.945846350845319),
+            TenxCsvValue::I32(19619),
+            TenxCsvValue::I32(321950603),
+            TenxCsvValue::I32(321950603),
+            TenxCsvValue::F64(0.4625052256865317),
+            TenxCsvValue::F64(0.9984212981890268),
+            TenxCsvValue::F64(0.8701900924844672),
+        ];
+
+        assert_eq!(parsed_data, expected_data);
     }
 }
